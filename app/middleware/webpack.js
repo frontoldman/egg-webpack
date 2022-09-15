a13e5780bb05eb0684a19cc1d398ffacc7548ccc
ran zhang <zr02718@ly.com>
2022-09-15 10:02:54 +0800

    初始开发

--- app/middleware/webpack.js
+++ app/middleware/webpack.js
@@ -1,12 +1,29 @@
 'use strict';
 
+const devMiddleware = require('koa-webpack-dev-middleware');
 const hotMiddleware = require('koa-webpack-hot-middleware');
-
 const webpack = require('webpack');
 
 module.exports = option => {
   const { configPath } = option;
   const devConfig = require(configPath);
   const compile = webpack(devConfig);
-  return hotMiddleware(compile);
+
+  const devWebpack = devMiddleware(compile, {
+    clientLogLevel: 'none',
+    stats: {
+      colors: true, // 控制台输出带颜色
+    },
+    publicPath: devConfig.output.publicPath,
+    hot: true,
+    open: true,
+    historyApiFallback: false,
+  });
+
+  const hotWebpack = hotMiddleware(compile);
+
+  return async function(ctx, next) {
+    await devWebpack(ctx, next);
+    await hotWebpack(next);
+  };
 };
